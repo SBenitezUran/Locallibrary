295d0c3ce08ad9232c8e87c0b424f2057893f328
santiago <santiagobenitez467@gmail.com>
2023-06-15 16:41:04 -0500

    feat: create static/css/styles.css

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render
 # Create your views here.
 
-from .models import Book, Author, BookInstance, Genre
+from .models.book import Book
+from .models.author import Author
+from .models.book_instance import BookInstance
+from .models.genre import Genre
+
 
 def index(request):
     """View function for home page of site."""
+    search_word = request.GET.get('search_word', '').lower()
+
 
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+    num_genres = Genre.objects.all().count()
 
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_genres = Genre.objects.filter(name__icontains=search_word).count()
+    num_books = Book.objects.filter(title__icontains=search_word).count()
+    
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
@@ -21,7 +31,11 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres' : num_genres
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+    
